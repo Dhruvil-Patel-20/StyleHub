f8378a44703c209f45eeb9a36ef22ec0cd0cee6d
Dhruvil Patel <126844702+Dhruvil-Patel-20@users.noreply.github.com>
2026-07-06 12:58:06 +0530

    Update API base URL to use environment variable

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,10 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
 
 api.interceptors.request.use((config) => {
-  const user = JSON.parse(localStorage.getItem('user') || '{}');
-  if (user.token) config.headers.Authorization = `Bearer ${user.token}`;
+  const user = JSON.parse(localStorage.getItem("user") || "{}");
+  if (user.token) {
+    config.headers.Authorization = `Bearer ${user.token}`;
+  }
   return config;
 });
 
